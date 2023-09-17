c246b50f6a8834c98a8c8c924cdd147b681a030e
rahmatsasongko <rahmat.sasongko24@gmail.com>
2023-09-17 15:48:23 +0700

    first commit

---
+++ cypress/e2e/demowebshop/demowebshop.cy.js
@@ -0,0 +1,34 @@
+import loginPage from '../../support/pageObject/userPage'
+const userr = require('../../fixtures/user.json')
+
+describe('test saucedemo', () => {
+  beforeEach(() => {
+    cy.visit('https://demowebshop.tricentis.com/login')
+  })
+  it(' multiple failed login', () => {
+    cy.fixture('fail-user.json').then((user) => {
+      user.failed_login.forEach((datauser) => {
+        cy.login(datauser.username,datauser.password)
+      })
+    })
+  })
+
+  
+  it('success login', () => {
+    loginPage.inputUsername(userr[0].username)
+    loginPage.inputPassword(userr[0].password)
+    loginPage.clickLoginButton()
+  })
+
+  it('C18 success login with fixtures', () => {
+    cy.fixture('user.json').then((user) => {
+      const datauser = user[0];
+      cy.login(datauser.username,datauser.password)
+      
+    })
+  })
+  it('C19 success login with custom command', () => {
+    cy.login('rahmat.sasongko@yopmail.com','Dummy123')
+    
+  })
+})
